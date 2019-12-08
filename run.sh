0839102a7d40ffdf780d8d96b4a194ac1d8e58a2
sunyiwei carbon <sunyiwei24601@163.com>
2019-12-07 22:40:23 -0500

    bar

---
+++ run.sh
@@ -0,0 +1,93 @@
+echo "python3 query.py results queryfile.txt bm25 single qtf 0.10" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf 0.25" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf 0.50" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf 0.50 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf 0.75" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf 0.75 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf 1.00" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf 1.00 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single idf 0.10" >> reports.txt
+python3 query.py results queryfile.txt bm25 single idf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single idf 0.25" >> reports.txt
+python3 query.py results queryfile.txt bm25 single idf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single idf 0.50" >> reports.txt
+python3 query.py results queryfile.txt bm25 single idf 0.50 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single idf 0.75" >> reports.txt
+python3 query.py results queryfile.txt bm25 single idf 0.75 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single idf 1.00" >> reports.txt
+python3 query.py results queryfile.txt bm25 single idf 1.00 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf*idf 0.10" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf*idf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf*idf 0.25" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf*idf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt bm25 single qtf*idf 0.50" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf*idf 0.50
+echo "python3 query.py results queryfile.txt bm25 single qtf*idf 0.75" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf*idf 0.75
+echo "python3 query.py results queryfile.txt bm25 single qtf*idf 1.00" >> reports.txt
+python3 query.py results queryfile.txt bm25 single qtf*idf 1.00
+
+echo "python3 query.py results queryfile.txt cosine single qtf 0.10" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf 0.25" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf 0.50" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf 0.50 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf 0.75" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf 0.75 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf 1.00" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf 1.00 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single idf 0.10" >> reports.txt
+python3 query.py results queryfile.txt cosine single idf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single idf 0.25" >> reports.txt
+python3 query.py results queryfile.txt cosine single idf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single idf 0.50" >> reports.txt
+python3 query.py results queryfile.txt cosine single idf 0.50 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single idf 0.75" >> reports.txt
+python3 query.py results queryfile.txt cosine single idf 0.75 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single idf 1.00" >> reports.txt
+python3 query.py results queryfile.txt cosine single idf 1.00 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf*idf 0.10" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf*idf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf*idf 0.25" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf*idf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt cosine single qtf*idf 0.50" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf*idf 0.50
+echo "python3 query.py results queryfile.txt cosine single qtf*idf 0.75" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf*idf 0.75
+echo "python3 query.py results queryfile.txt cosine single qtf*idf 1.00" >> reports.txt
+python3 query.py results queryfile.txt cosine single qtf*idf 1.00
+
+echo "python3 query.py results queryfile.txt lm single qtf 0.10" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf 0.25" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf 0.50" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf 0.50 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf 0.75" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf 0.75 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf 1.00" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf 1.00 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single idf 0.10" >> reports.txt
+python3 query.py results queryfile.txt lm single idf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single idf 0.25" >> reports.txt
+python3 query.py results queryfile.txt lm single idf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single idf 0.50" >> reports.txt
+python3 query.py results queryfile.txt lm single idf 0.50 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single idf 0.75" >> reports.txt
+python3 query.py results queryfile.txt lm single idf 0.75 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single idf 1.00" >> reports.txt
+python3 query.py results queryfile.txt lm single idf 1.00 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf*idf 0.10" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf*idf 0.1 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf*idf 0.25" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf*idf 0.25 >> reports.txt
+echo "python3 query.py results queryfile.txt lm single qtf*idf 0.50" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf*idf 0.50
+echo "python3 query.py results queryfile.txt lm single qtf*idf 0.75" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf*idf 0.75
+echo "python3 query.py results queryfile.txt lm single qtf*idf 1.00" >> reports.txt
+python3 query.py results queryfile.txt lm single qtf*idf 1.00
+
